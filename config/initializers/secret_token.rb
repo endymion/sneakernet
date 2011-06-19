504a5205e378ad6b2af58cd5c1d74fe05a7007c8
Ryan Alyn Porter <rap@endymion.com>
2011-06-18 22:11:03 -0400

    Generated by Rails 3.0.9, 'rails new sneakernet'.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sneakernet::Application.config.secret_token = '811694956c7757ca8a96cbc79b975b25f3f7e99d2d52ff27c5c05cb3f77c7cc31fc3f627084d34f6ddb17037b6742a14c23d8d5febdc5e1eb2c6912e587815e0'
